8bdb1a9c8d3066c3cd9afe14d14a6c5864307694
Victor Kasap <kasap.victor@gmail.com>
2022-11-26 18:49:45 +0200

    feat: Added Force Rerend Map

--- src/entities/country/components/details/Map.jsx
+++ src/entities/country/components/details/Map.jsx
@@ -1,19 +1,34 @@
 import PropTypes from 'prop-types';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
 
 import { MapBox } from './styled';
 
+function ChangeMapView({ coords }) {
+  const map = useMap();
+  map.setView([coords[0], coords[1]], map.getZoom());
+
+  return null;
+}
+
+ChangeMapView.propTypes = {
+  coords: PropTypes.array,
+};
+
 export const Map = ({ countryById }) => {
   const { countryName, capitalName, coords } = countryById;
+  const coordsData = [coords[1][0], coords[1][1]];
 
   return (
     <MapBox>
-      <MapContainer center={[coords[1][0], coords[1][1]]} zoom={10} scrollWheelZoom={true}>
+      <MapContainer center={coordsData} zoom={8} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[coords[1][0], coords[1][1]]}>
+
+        <ChangeMapView coords={coordsData} />
+
+        <Marker position={coordsData}>
           <Popup>
             {countryName[1]}, {capitalName[1]}
           </Popup>
